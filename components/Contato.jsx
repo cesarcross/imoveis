c83ac110bbea566e6ed7cc59d9306dbfabf29070
Vinicius Alvim <vini0191@gmail.com>
2021-04-19 15:49:26 -0300

    fix: small improvement in form and modify slightly the WhatsApp img

--- components/Contato.jsx
+++ components/Contato.jsx
@@ -6,7 +6,7 @@ import styles from '../styles/Contato.module.scss';
 import Image from 'next/image';
 
 const messageSent = () =>
-  toast.success(`Mensagem enviada com sucesso!`, {
+  toast.success('Mensagem enviada com sucesso!', {
     position: 'bottom-center',
     autoClose: 3000,
     hideProgressBar: false,
@@ -15,7 +15,7 @@ const messageSent = () =>
   });
 
 const messageFailed = () =>
-  toast.error(`Por favor preencha todos os campos`, {
+  toast.error('Desculpe-nos, houve algum problema.', {
     position: 'bottom-center',
     autoClose: 3000,
     hideProgressBar: false,
@@ -28,58 +28,56 @@ const Contato = () => {
     <Formik
       initialValues={{ name: '', email: '', message: '' }}
       validationSchema={Yup.object({
-        name: Yup.string(),
+        name: Yup.string().required('"Nome" é um campo necessário.'),
         // .max(15, 'Must be 15 characters or less')
         // .required(),
-        email: Yup.string().email('Invalid email address'),
+        email: Yup.string()
+          .required('"Email" é um campo necessário.')
+          .email('Endereço de email inválido'),
         // .required()
-        message: Yup.string(),
+        message: Yup.string().required('"Mensagem" é um campo necessário.'),
         // .required()
       })}
       onSubmit={(values, { setSubmitting }) => {
-        console.log('sent', JSON.stringify(values, null, 2));
         axios
           .post('/api/mail', values)
           .then(function (response) {
-            console.log(response);
+            console.log({ response });
+            console.log('sent', JSON.stringify(values, null, 2));
             messageSent();
           })
           .catch(function (error) {
-            console.log(error);
-            const { name, email, message } = values;
-            if (name || email || message === '') {
-              return messageFailed();
-            }
+            console.log({ error });
+            messageFailed();
           });
+
         setSubmitting(false);
       }}
     >
-      <div className={styles.container} id='contact'>
+      <div className={styles.container} id="contact">
         <Form className={styles.form}>
           <h2>Entre em contato</h2>
 
-          <label htmlFor='name'>Nome</label>
-          <Field name='name' type='text' />
-          <ErrorMessage name='name' />
+          <label htmlFor="name">Nome</label>
+          <Field name="name" type="text" />
+          <ErrorMessage name="name" />
 
-          <label htmlFor='email'>Email</label>
-          <Field name='email' type='email' />
-          <ErrorMessage name='email' />
+          <label htmlFor="email">Email</label>
+          <Field name="email" type="email" />
+          <ErrorMessage name="email" />
 
-          <label htmlFor='message'>Mensagem</label>
-          <Field name='message' component='textarea' rows='10' />
-          <ErrorMessage name='message' />
+          <label htmlFor="message">Mensagem</label>
+          <Field name="message" component="textarea" rows="10" />
+          <ErrorMessage name="message" />
 
           <div className={styles.mobilewhats}>
-            <a href='https://wa.me/5511981263581' target='_blank'>
+            <a href="https://wa.me/5511981263581" target="_blank">
               {/* Image não deve ser children de <p>. Refatorar */}
-              <p>
-                Ou escreva pelo Whatsapp{' '}
-                <Image src='/whats.png' width={50} height={50} />
-              </p>
+              <p>Ou escreva pelo Whatsapp </p>
+              <Image src="/whats.png" width="50" height="50" layout="fixed" />
             </a>
 
-            <button type='submit'>Enviar</button>
+            <button type="submit">Enviar</button>
           </div>
         </Form>
       </div>
